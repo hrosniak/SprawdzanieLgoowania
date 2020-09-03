3410031c0b19981b32244011ad32296031afb2d1
Hubert_Rosniak <hubertcfc@gmail.com>
2020-09-03 18:24:06 +0200

    LoginTestCompare

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,20 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String login = "Portfel123";
         String password = "Woda2020";
 
 
-        Scanner sc = new Scanner(System.in);
-
 
         System.out.println("Tap your login: ");
-        String lo = sc.nextLine();
+        String lo = sc.nextLine() ;
+
 
         System.out.println("Tap your password: ");
-        String pas = sc.nextLine();
+        String pas = sc.nextLine() ;
 
-
-        if (login == lo && password == pas) {
+        if (lo.equals(login) && pas.equals(password)) {
             System.out.println("Login succesfull");
         } else
             System.out.println("Try again!!!");
